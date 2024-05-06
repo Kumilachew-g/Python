d30b6195155b46921c8da069a3a9134f7c466a31
Kumilachew-g <kumilachew1212@gmail.com>
2024-05-06 17:27:56 +0300

    Add even or odd funtion with python

--- python.py
+++ python.py
@@ -13,3 +13,22 @@ def area_rectangle(length, width):
 area = area_rectangle(5, 6)
 print("Area of the rectangle:", area)
 
+
+print("Even or Odd function with python")
+
+def even_or_odd(number):
+    if number % 2 ==0:
+        return "Even"
+    else:
+        return "Odd"
+num = 8
+result =even_or_odd(num)
+print(f"The number {num} is {result}")
+# f is used to format the result. The output will beThe number {num} is {result} in python without f key word
+print("Even or Odd function with python more...")
+
+def even_or_odd(number):
+    return "Even" if num % 2 ==0 else "Odd"
+num = 7
+result = even_or_odd(num)
+print(f"The number {num} is {result}")
