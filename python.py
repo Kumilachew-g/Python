f232baf749f5f4dcc0d9cd07b700bd835bee7c77
Kumilachew-g <kumilachew1212@gmail.com>
2024-05-06 17:56:27 +0300

    Enhance

--- python.py
+++ python.py
@@ -50,9 +50,9 @@ else:
 # 4 Factorial using python function
 
 def factorial(n):
-    if n <0:
+    if n < 0:
         raise ValueError("Factorial is not defined for negative numbers")
-    if n ==0:
+    if n == 0:
         return 1
     else:
         return n*factorial(n-1)
