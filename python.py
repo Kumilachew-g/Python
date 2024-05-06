00b16ebef4dcb20778d0c68c23f9e610dc3ea01c
Kumilachew-g <kumilachew1212@gmail.com>
2024-05-06 17:14:45 +0300

    Add triangle area with python function

---
+++ python.py
@@ -0,0 +1,15 @@
+print("Triangle area with python")
+
+def area_triangle(length, width):
+    area = length * width
+    return area
+area = area_triangle(5, 6)
+print("The area of the rectangle is: ", area)
+
+print("Triangle area with python more...")
+
+def area_rectangle(length, width):
+    return length * width
+area = area_rectangle(5, 6)
+print("Area of the rectangle:", area)
+
